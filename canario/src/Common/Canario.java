793f8c86a251b306ef1f5597dc9765ed043a9f7e
Dario Gonzalez <darioegb@gmail.com>
2012-12-04 12:02:42 -0300

    cambios

--- canario/src/Common/Canario.java
+++ canario/src/Common/Canario.java
@@ -2,17 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package canario;
+package Common;
 
-/**
- *
- * @author Bruno
- */
+
 public class Canario {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
